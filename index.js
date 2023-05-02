1f28c9579ad350725fbadc9bb79974b7ae432077
dymovv1 <romandymovv1997@gmail.com>
2023-05-02 15:48:06 +0200

    new

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
- "use strict"
- 
- const apiKey = '5a7411ae33c02e61a10837fe90d7fe85';
-  const searchInput = document.getElementById('search-input');
-  const typeSelect = document.getElementById('type-select');
-  const searchButton = document.getElementById('search-button');
-  const resultsContainer = document.getElementById('results');
 
-  function search() {
-    const query = searchInput.value;
-    const type = typeSelect.value;
+"use strict";
 
-    if (query.trim() === '') {
-      alert('Введіть назву фільму/серіалу');
-      return;
-    }
+const apiKey = "5a7411ae33c02e61a10837fe90d7fe85";
+const searchInput = document.getElementById("search-input");
+const typeSelect = document.getElementById("type-select");
+const searchButton = document.getElementById("search-button");
+const resultsContainer = document.getElementById("results");
 
-    if (type === '') {
-      alert('Оберіть тип фільму/серіалу');
-      return;
-    }
+function search() {
+const query = searchInput.value;
+const type = typeSelect.value;
 
-    resultsContainer.innerHTML = '';
+if (query.trim() === "") {
+alert("Введіть назву фільму/серіалу");
+return;
+}
 
-    fetch(`https://api.themoviedb.org/3/movie/550?api_key=${apiKey}&s=${query}&type=${type}`)
-      .then(response => response.json())
-      .then(data => {
-        if (data.Response === 'False') {
-          alert('Нічого не знайдено');
-          return;
-        }
+resultsContainer.innerHTML = "";
 
-        const resultsHtml = data.Search.map(item => {
-          return `
-            <div>
-              <h3>${item.Title}</h3>
-              <img src="${item.Poster}">
-              <p>Рік: ${item.Year}</p>
-              <p>Тип: ${item.Type}</p>
-              <p>IMDb ID: ${item.imdbID}</p>
-            </div>
-          `;
-        }).join('');
+const url = type && type !== 'Обери тип :' ?
+  `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${query}&media_type=${type}&page=1` :
+  `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${query}&media_type=tv&page=1`;
 
-        resultsContainer.innerHTML = resultsHtml;
-      })
-      .catch(error => {
-        alert('Під час виконання запиту сталася помилка');
-        console.error(error);
-      });
-  }
 
-  searchButton.addEventListener('click', search);
+fetch(url)
+.then((response) => response.json())
+.then((data) => {
+if (data.results.length === 0) {
+alert("Нічого не знайдено");
+return;
+}
+
+const items = data.results.filter(item => item.media_type === type).slice(0, 10);
+const resultsHtml = items
+  .map((item) => {
+    return `
+    <div>
+      <h3>${item.title || item.name}</h3>
+      <img src="https://image.tmdb.org/t/p/w500${item.poster_path}">
+      <p>Рік: ${item.release_date || item.first_air_date}</p>
+      <p>Тип: ${item.media_type}</p>
+      <p>IMDb ID: ${item.id}</p>
+    </div>
+    `;
+  })
+  .join("");
+
+
+resultsContainer.innerHTML = resultsHtml;
+})
+.catch((error) => {
+alert("Під час виконання запиту сталася помилка");
+console.error(error);
+});
+
+}
+
+searchButton.addEventListener("click", search);
+typeSelect.addEventListener("change", search);
